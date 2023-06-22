d4d474613a46484284a90bdf1be6eec19fb0ac4b
cansuhugul18 <cansu18.hugul@gmail.com>
2023-06-23 00:56:34 +0300

    Proje dosyası ekle.

---
+++ Web_mysql/WebForm1.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Web_mysql.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="tablegridview" runat="server">
+        <Columns>
+        <asp:BoundField DataField="kisi_id" HeaderText="kisi_id" />
+        <asp:BoundField DataField="kisi_adi" HeaderText="kisi_adi" />
+     
+
+    </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Web_mysql/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Web_mysql
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("Server=localhost;Database=my_schema;Uid=root;Pwd=Chbcd12012018;");
+            mySqlConnection.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from kisiler", mySqlConnection);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt); adapter.Dispose();mySqlConnection.Dispose();
+            tablegridview.DataSource = dt;
+            tablegridview.DataBind();
+            dt.Dispose();
+
+        }
+    }
+}
